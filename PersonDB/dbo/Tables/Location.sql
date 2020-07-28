c2b2d7108550108f9b40ac33689bb9fa463f754c
IyadShobaki <iyadshobaki82@gmail.com>
2020-07-28 10:22:46 -0400

    Handle Database External Changes

--- PersonDB/dbo/Tables/Location.sql
+++ PersonDB/dbo/Tables/Location.sql
@@ -1,6 +1,9 @@
-﻿CREATE TABLE [dbo].[Location]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [LocationName] NVARCHAR(50) NOT NULL, 
-    [Location] NVARCHAR(50) NOT NULL
-)
+﻿CREATE TABLE [dbo].[Location] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [LocationName] NVARCHAR (50) NOT NULL,
+    [Location]     NVARCHAR (50) NOT NULL,
+    [Country]      NVARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
